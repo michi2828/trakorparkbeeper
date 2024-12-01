261224ad968e5494be622e1d681fa0e29f445ca4
michi2828 <michi.dahlmann@icloud.com>
2024-12-01 11:21:01 +0100

    newping rewrite
    newping rewrite for better calculations + added diagramm for wokwi

--- Parkpiepser1.0/Parkpiepser1.0.ino
+++ Parkpiepser1.0/Parkpiepser1.0.ino
@@ -1,127 +1,86 @@
-int piezo = 9;
-int distance = 0;
-int distance2 = 0;
-int distance3 = 0;
-int duration = 0;
-int duration2 = 0;
-int duration3 = 0;
+#include <NewPing.h>
 
-unsigned long previousMillis1 = 0; 
+#define MAX_DISTANCE 300
+
+// Pins for each sensor
+#define TRIG_PIN1 8
+#define ECHO_PIN1 2
+#define TRIG_PIN2 3
+#define ECHO_PIN2 4
+#define TRIG_PIN3 5
+#define ECHO_PIN3 6
+int beep = 0;
+int beep1 = 0;
+int beep2 = 0;
+int beep3 = 0;
+int nbcount = 0;
+NewPing sonar1(TRIG_PIN1, ECHO_PIN1, MAX_DISTANCE);
+NewPing sonar2(TRIG_PIN2, ECHO_PIN2, MAX_DISTANCE);
+NewPing sonar3(TRIG_PIN3, ECHO_PIN3, MAX_DISTANCE);
+
+unsigned long previousMillis1 = 0;
 unsigned long previousMillis2 = 0;
 unsigned long previousMillis3 = 0;
-const long interval1 = 0; 
-const long interval2 = 0;
-const long interval3 = 0;
-
-#include <NewPing.h>
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-// ultraschall Sensor
-const int trigPin1 = 1;
-const int echoPin1 = 2;
-const int trigPin2 = 3;
-const int echoPin2 = 4;
-const int trigPin3 = 5;
-const int echoPin3 = 6;
+const long interval = 1000;  // Delay between each ping in milliseconds
 
 void setup() {
+  Serial.begin(9600);
 
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(trigPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
- Serial.begin(9600);
 }
-
-
 
 void loop() {
+  unsigned long currentMillis = millis();
 
-    unsigned long currentMillis = millis();
- delay(100);
-    // Funktion 1
-    if (currentMillis - previousMillis1 >= interval1) {
-        previousMillis1 = currentMillis;
-        function1();
+//if (beep1)
+
+
+
+
+
+
+  // Sensor 1
+ if (currentMillis - previousMillis1 >= interval) {
+    previousMillis1 = currentMillis;
+    unsigned int distance1 = sonar1.ping_cm();
+    Serial.print("Sensor 1: ");
+    if (distance1 == 0) {
+      Serial.println("Außer Reichweite");
+        beep1 = 0;
+    } else {
+      Serial.println("In Reichweite");
+      beep1 = distance1;
     }
- delay(100);
-    // Funktion 2
-    if (currentMillis - previousMillis2 >= interval2) {
-        previousMillis2 = currentMillis;
-        function2();
-    }
-delay(100);
-    // Funktion 3
-    if (currentMillis - previousMillis3 >=interval3) {
-      previousMillis3 = currentMillis;
-      function3();
-    }
-
-
-
-
-}
-
-void function1()
-{
-digitalWrite(trigPin1, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW);
-
-
-  long duration = pulseIn(echoPin1, HIGH);
-  long distance = duration * 0.034 / 2;
+  }
   
-Serial.print("1s");
-Serial.println(distance);
+
+
+  // Sensor 2
+  if (currentMillis - previousMillis2 >= interval) {
+    previousMillis2 = currentMillis;
+    unsigned int distance2 = sonar2.ping_cm();
+    Serial.print("Sensor 2: ");
+    if (distance2 == 0) {
+      Serial.println("Außer Reichweite");
+         beep2 = 0;
+    } else {
+      Serial.println("In Reichweite");
+      beep2 = distance2;
+    }
+  }
+
+
+
+  // Sensor 3
+  if (currentMillis - previousMillis3 >= interval) {
+    previousMillis3 = currentMillis;
+    unsigned int distance3 = sonar3.ping_cm();
+    Serial.print("Sensor 3: ");
+    if (distance3 == 0) {
+      Serial.println("Außer Reichweite");
+      beep3 = 0;
+    } else {
+      Serial.println("In Reichweite");
+      beep3 = distance3;
+    }
+  }
 }
-
-
-
-void function2 ()
- {
-//blinkende lämpchen
-
-
-digitalWrite(trigPin2, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin2, LOW);
-
-
-  long duration = pulseIn(echoPin2, HIGH);
-  long distance2 = duration * 0.034 / 2;
-  
-Serial.print("2s");
-Serial.println(distance2);
-
-
- }
-
-
-
-void function3()
-{
-digitalWrite(trigPin3, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin3, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin3, LOW);
-
-
-  long duration = pulseIn(echoPin3, HIGH);
-  long distance3 = duration * 0.034 / 2;
-
- Serial.print("3s");
-Serial.println(distance3); 
- 
-}
-
-
